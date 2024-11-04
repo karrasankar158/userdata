e67d98d1e36e4ff28faf13ef448ee26120a3a25b
karrasankar158 <karrasankar09@gmail.com>
2024-11-04 14:06:02 +0530

    new objects added

--- src/main/java/com/fsd/service/UserDataService.java
+++ src/main/java/com/fsd/service/UserDataService.java
@@ -11,7 +11,9 @@ public class UserDataService {
 
 	public List<UserData> getAllUsers(){
 		return List.of(new UserData(101,"Sankar","A.P"),
-				new UserData(102,"chandu","Hyderabad"));
+				new UserData(102,"chandu","Hyderabad"),
+				new UserData(103,"sam","U.P"),
+				new UserData(104,"kumar","A.p"));
 				
 	}
 }
